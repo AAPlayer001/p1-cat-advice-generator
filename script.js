0142ef93023cdd413d2a719c583028e15f42805e
Daniel Thomas <danielhead0376@gmail.com>
2024-01-11 14:28:34 -0500

    added 1 HTML edit and JS

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Deez nuts');
+function getAdvice() {
+    fetch("https://api.adviceslip.com/advice")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('advice').textContent = data.slip.advice;
+        })
+        .catch(error => {
+            console.error("Error retrieving advice:", error);
+            document.getElementById('advice').textContent = "Failure, cannot load advice.";
+        });
+}
+
+function getCatImage() {
+    
+    document.getElementById('catImage').src = 'https://cataas.com/cat';
+}
+
+document.getElementById('adviceButton').addEventListener('click', function() {
+    getAdvice();
+    getCatImage();
+})
